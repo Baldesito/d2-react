7d69205afbedbcd4ebc5005c7445224cc2b86895
OCCHIO NERO <112170802+Baldesito@users.noreply.github.com>
2024-11-26 15:18:41 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap";
+import CustomNavbar from "./components/CustomNavbar";
 
 function App() {
   return (
     <div className="App">
+      <CustomNavbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
